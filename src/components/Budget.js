18b5ed37c257967cdf8e92bc2ef30cd59e0b49ac
unknown <akachai1624@gmail.com>
2024-05-02 21:10:31 +0700

    Updated the HR did not work

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,18 +1,17 @@
-import React, {useContext, useState} from 'react';
-import {AppContext} from '../context/AppContext';
+import React, { useContext, useState } from 'react';
+import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
     const { budget } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
-    const handleBudgetChage = (event) => {
+    const handleBudgetChange = (event) => {
         setNewBudget(event.target.value);
     }
     return (
-        <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
-            <input type="number" step="10" value={newBudget} onChange={handleBudgetChage}></input>
-        </div>
+<div className='alert alert-secondary'>
+<span>Budget: £{budget}</span>
+<input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
+</div>
     );
 };
-
 export default Budget;
